cf3494079e42d8ee642e8efb1942a99cbb477412
Abdallah Mohamed <abdallah.mohamed22@msa.edu.eg>
2021-11-13 15:13:37 +0200

    add InvoiceDetails class

---
+++ Models/InvoiceDetails.php
@@ -0,0 +1,39 @@
+<?php
+    protected $Id;
+    protected $ItemID;
+    protected $InvoiceID;
+    protected $Quantity;
+    protected $Total;
+    public function __construct()
+    {
+
+    }
+    public function getID()
+    {
+        return $this->id;
+    }
+    public function getItemID()
+    {
+        return $this->ItemID;
+    }
+    public function getInvoiceID()
+    {
+        return $this->InvoiceID;
+    }
+    public function setQuantity(int $Quantity)
+    {
+        $this->Quantity = $Quantity;
+    }
+    public function getQuantity()
+    {
+        return $this->Quantity;
+    }
+    public function Calculate()
+    {       
+    }
+    public function getTotal(int $Total)
+    {
+        return $this->Total;  
+    }
+}
+?>
