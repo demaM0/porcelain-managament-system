1c7cd620506ae58afaf103671d9740f362965980
selltea <deemadee0302@gmail.com>
2021-11-13 13:29:59 +0200

    update user model class

--- Models/User.php
+++ Models/User.php
@@ -1,3 +1,28 @@
 <?php
+include_once("Person.php");
 
+abstract class User extends Person {
+
+    protected $username;
+    protected $password;
+
+    abstract public function setusername(string $username)
+    {
+        $this->username = $username;
+    }
+    abstract public function setpassword(string $password)
+    {
+        $this->password = $password;
+    }
+
+    abstract public function getusername()
+    {
+        return $this->username;
+    }
+
+    abstract public function getpassword()
+    {
+        return $this->password;
+    }
+  }
 ?>
