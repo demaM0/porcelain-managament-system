7e853cb8ba18ebc60b95d9d1a0619e203399648e
KareemSabryKhaled <kareem_khaled2001@hotmail.com>
2021-11-13 12:58:08 +0200

    Added Every set and get class and fixed the mistake

--- Models/Item.php
+++ Models/Item.php
@@ -1,18 +1,55 @@
 <?php
 class item(){
-   $id;
-   $name;
-   $color;
-   $quantity;
-   $itemTypeId;
-   $price;
-    public function SetId()
-    {
-
-    }
+   protected $id;
+   protected $name;
+   protected $color;
+   protected $quantity;
+   protected $itemTypeId;
+   protected $price;
+    
     public function getId()
     {
-
+        return $this->id;
+    }
+    public function setName($name)
+    {
+        $this->name=$name;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function setColor($color)
+    {
+        $this->color=$color;
+    }
+    public function getColor()
+    {
+        return $this->color;
+    }
+    public function setQuantity($quantity)
+    {
+        $this->quantity=$quantity;
+    }
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+    public function setItemTypeId($itemTypeId)
+    {
+        $this->itemTypeId=$itemTypeId;
+    }
+    public function getItemTypeId()
+    {
+        return $this->ItemTypeId;
+    }
+    public function setPrice($price)
+    {
+        $this->price=$price;
+    }
+    public function getPrice()
+    {
+        return $this->Price;
     }
 
 }
