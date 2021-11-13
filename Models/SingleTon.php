586dfc67c448b9d0e0ea1f07cdd45f01f99707ee
KareemSabryKhaled <kareem_khaled2001@hotmail.com>
2021-11-13 16:31:45 +0200

    Add Singlton file

---
+++ Models/SingleTon.php
@@ -0,0 +1,59 @@
+<?php
+class DbConnection
+ {
+    private $host="localhost";
+    private $username="root";
+    private $password="";
+    private $db_Name="almasrya";
+    private $db_connection;
+    private static $instance;
+    public static $counter=0;
+    private function __construct()
+    {
+        $this->db_connection=$this->database_connect($this->host,$this->username,$this->password,$this->db_Name);
+
+    }
+    public static function getInstance()
+    {
+        if(self::$instance==null)
+        {
+            echo "Return New Instance";
+            self::$instance=new DbConnection();
+        }
+        else
+        {
+            echo "Object is their <br>";  
+        }
+        return self::$instance;
+
+    }
+    private function database_connect($database_host,$database_username,$database_password,$db_Name)
+    {
+        if($connection=mysqli_connect($database_host,$database_username,$database_password,$db_Name))
+        {
+            return $connection;
+        }
+        else
+        {
+            die("Database connection error");
+        } 
+        
+    }
+    /** 
+     * 
+     * @param string $db_Name
+     * @return mysql link
+    */
+    private function database_select($db_Name)
+    {
+        return mysqli_select_db($this->database_connection,$db_Name)
+        or die("no database is selected");
+
+    }
+    
+}
+for($i=0;$i<100;$i++)
+    {
+        $db=DbConnection::getInstance();
+    }
+?>
