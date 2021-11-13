87d2d6ed6ddb064d58fcd7a9eb9a4487fa0c1336
selltea <deemadee0302@gmail.com>
2021-11-13 19:31:11 +0200

    update Transaction model

--- Models/Transaction.php
+++ Models/Transaction.php
@@ -6,9 +6,22 @@ class Transaction
     protected $DateOfTransaction;
     protected $SupplierID;
     protected $ManagerID;
-    public function __construct()
+    public function __construct($Id)
     {
-
+        $con = mysqli_connect("localhost","root","","almasrya");
+        if(!$con)
+        {
+          die('could not connect: ' . mysqli_error());
+        }
+        $sql="select * from Transaction where Id=$Id";
+        $Transactiondataset = mysqli_query($con,$sql);
+        if($row = mysqli_fetch_array($Transactiondataset))
+        {
+          $this->Id=$row["Id"];
+          $this->FullPrice=$row["FullPrice"];
+          $this->DateOfTransaction=$row["DateOfTransaction"];
+          $this->SupplierID=$row["ManagerID"];
+        }
     }
     public function getID()
     {
