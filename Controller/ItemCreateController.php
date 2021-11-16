02ed7e285517c5dcb08f045cd210fd7853a7f97c
Abdallah Mohamed <abdallah.mohamed22@msa.edu.eg>
2021-11-16 02:45:26 +0200

    createitemview done

--- Controller/ItemCreateController.php
+++ Controller/ItemCreateController.php
@@ -1,8 +1,9 @@
 <?php
 require_once("SingleTon.php");
-require_once("notify.php");
+require_once("\Views\CreateItemView\connectcreate.php");
 class CreateItem
 {
+
 
     public function __construct($name,$color,$quantity,$type,$price,$supplierid)
     {
@@ -11,7 +12,7 @@ class CreateItem
         {
           die('could not connect: ' . mysqli_error($con));
         }
-        $reg = "insert into items(Name, Color, Quantity, Type, Price, SupplierID) values ('$name', '$color', '$quantity', '$type', '$price', '$supplierid')";
+        $reg = "insert into items(Name, Color, Quantity, Type, Price, SupplierID) values ('$name', '$color', $quantity, $type, $price, $supplierid)";
         mysqli_query($con,$reg);
         
     }
