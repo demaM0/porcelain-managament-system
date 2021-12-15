ae674bb889ba8b53ab8a50411bb8819568af136d
KareemSabryKhaled <kareem_khaled2001@hotmail.com>
2021-12-15 22:21:32 +0200

    address updated working mvc

--- Models/SingleTon.php
+++ Models/SingleTon.php
@@ -17,13 +17,10 @@ class DbConnection
     {
         if(self::$instance==null)
         {
-            echo "Return New Instance";
+            
             self::$instance=new DbConnection();
         }
-        else
-        {
-            echo "Object is their <br>";  
-        }
+        
         return self::$instance->db_connection;
 
     }
