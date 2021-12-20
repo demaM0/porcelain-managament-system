50b210c9387e80b9ecf7217ba0311ae0d2904689
shaymaa-hm <shaymaamostafa50@gmail.com>
2021-12-20 23:00:00 +0200

    installment transaction m2m

--- Controller/Installment/installment-create-controller.php
+++ Controller/Installment/installment-create-controller.php
@@ -4,22 +4,10 @@
 	$Quantity = $_POST['Quantity'];
 	$TransactionId = $_POST['TransactionId'];
 	
-	$con =DbConnection::getInstance();
-        if(!$con)
-        {
-          die('could not connect: ' . mysqli_error($con));
-        }
-	$s = "select * from transaction where Id  = '$TransactionId' ";
-	$result = mysqli_query($con,$s);
-	$num = mysqli_num_rows($result);
-
-	if($num==1){
-		installment::create($Quantity);
 	
-		installment::assigntransaction($TransactionId);
-	}
-	else{
-	echo"this transaction doesnt exist";
-	}  
+		installment::create($Quantity,$TransactionId);
+	
+		installment::transactioninstallment($TransactionId);
+	
 	
 ?>
