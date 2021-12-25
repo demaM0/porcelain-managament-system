55fc7171bff84161cb87bc63aca9b3b5d0856933
KareemSabryKhaled <kareem_khaled2001@hotmail.com>
2021-12-25 02:49:20 +0200

    add translate to mvc

--- Controller/Translation/Translation.php
+++ Controller/Translation/Translation.php
@@ -1,36 +1,14 @@
 <?php
 require_once('../../Models/SingleTon.php');
+require_once('../../Models/translation-model.php');
 header('Content-Type: application/json');
 $results = array();
-    
-$results['result'] = Translate( $_GET['lang_code'],$_GET['trans_key'] );
+$translate=new Translate();
+$results['result'] = $translate->translate( $_GET['lang_code'],$_GET['trans_key'] );
+
 $json=json_encode($results);
-
 echo($json);
-function Translate($lang_code, $trans_key)
-{
-  
-  
-    $con =DbConnection::getInstance();
-    if(!$con)
-    {
-      die('could not connect: ' . mysqli_error($con));
-    }
-    // select  id from translation where TranslationName =  $trans_key
 
-    $sql="select word from translationdetails inner join translation on translation.TranslationName = '$trans_key' where translationdetails.LangId = $lang_code and translationdetails.TransId=translation.Id ";
-    $result = mysqli_query($con,$sql);
-    
-    if ($result->num_rows > 0) {
-        if($row = mysqli_fetch_array($result))
-        {
-            return $row["word"];
-        }else 
-        return "row no results";
-
-    }else 
-    return "no results";
-}
 
 
 
---
+++ Models/translation-model.php
@@ -0,0 +1,25 @@
+<?php
+require_once("SingleTon.php");
+class Translate{
+    public function translate($lang_code, $trans_key)
+    {
+        
+        $con = DbConnection::getInstance();
+        if (!$con) {
+            die('could not connect: ' . mysqli_error($con));
+        }
+        // select  id from translation where TranslationName =  $trans_key
+
+        $sql = "select word from translationdetails inner join translation on translation.TranslationName = '$trans_key' where translationdetails.LangId = $lang_code and translationdetails.TransId=translation.Id ";
+        $result = mysqli_query($con, $sql);
+
+        if ($result->num_rows > 0) {
+            if ($row = mysqli_fetch_array($result)) {
+                return $row["word"];
+            } else
+                return "row no results";
+        } else
+            return "no results";
+    }
+}
+?>
